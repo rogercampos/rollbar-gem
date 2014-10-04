eefa32b86cb580755bb568ad3c160694ee7acf54
Roger Campos <roger@itnig.net>
2014-10-04 18:48:59 +0200

    Get the exception set by action dispatch from env instead of monkeypatching

--- lib/rollbar/middleware/rails/show_exceptions.rb
+++ lib/rollbar/middleware/rails/show_exceptions.rb
@@ -1,31 +1,25 @@
 module Rollbar
   module Middleware
     module Rails
-      module ShowExceptions
+      class ShowExceptions
         include ExceptionReporter
 
-        def render_exception_with_rollbar(env, exception)
-          key = 'action_dispatch.show_detailed_exceptions'
+        def initialize(app)
+          @app = app
+        end
 
-          # don't report production exceptions here as it is done below
-          # in call_with_rollbar() when show_detailed_exception is false
-          if not env.has_key?(key) or env[key]
-            report_exception_to_rollbar(env, exception)
+        def call(env)
+          @app.call(env).tap do |_|
+            if exception = env["action_dispatch.exception"]
+              key = 'action_dispatch.show_detailed_exceptions'
+
+              # don't report production exceptions here as it is done below
+              # in call_with_rollbar() when show_detailed_exception is false
+              if not env.has_key?(key) or env[key]
+                report_exception_to_rollbar(env, exception)
+              end
+            end
           end
-          render_exception_without_rollbar(env, exception)
-        end
-
-        def call_with_rollbar(env)
-          call_without_rollbar(env)
-        rescue Exception => exception
-          # won't reach here if show_detailed_exceptions is true
-          report_exception_to_rollbar(env, exception)
-          raise exception
-        end
-
-        def self.included(base)
-          base.send(:alias_method_chain, :call, :rollbar)
-          base.send(:alias_method_chain, :render_exception, :rollbar)
         end
       end
     end
